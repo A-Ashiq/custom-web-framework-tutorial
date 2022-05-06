785ee7256d2083fde6073014099988862649c3fb
afaanashiq <afaan.ashiq@eigentech.com>
2022-05-06 19:45:11 +0100

    add basic routing + fallback request handler

--- api.py
+++ api.py
@@ -1,18 +1,46 @@
-from webob import Request, Response
+import webob
+import parse
 
 
 class API:
+    def __init__(self):
+        self.routes = {}
+
     def __call__(self, environ, start_response):
-        request = Request(environ)
+        request = webob.Request(environ)
 
         response = self.handle_request(request)
 
         return response(environ, start_response)
 
     def handle_request(self, request):
-        user_agent = request.environ.get("HTTP_USER_AGENT", "No User Agent Found")
+        response = webob.Response()
 
-        response = Response()
-        response.text = f"Hello, my friend with this user agent: {user_agent}"
+        handler, kwargs = self.find_handler(request_path=request.path)
+
+        if handler is not None:
+            handler(request, response, **kwargs)
+        else:
+            self.default_response(response)
 
         return response
+
+    def default_response(self, response):
+        response.status_code = 404
+        response.text = "Not found."
+        return response
+
+    def find_handler(self, request_path):
+        for path, handler in self.routes.items():
+            parse_result = parse.parse(path, request_path)
+            if parse_result is not None:
+                return handler, parse_result.named
+
+        return None, None
+
+    def route(self, path):
+        def wrapper(handler):
+            self.routes[path] = handler
+            return handler
+
+        return wrapper
--- app.py
+++ app.py
@@ -3,5 +3,12 @@ from api import API
 app = API()
 
 
+@app.route("/home")
 def home(request, response):
     response.text = "Hello from the HOME page"
+
+
+
+@app.route("/hello/{name}")
+def hello(request, response, name):
+    response.text = f"Hello, {name}"
