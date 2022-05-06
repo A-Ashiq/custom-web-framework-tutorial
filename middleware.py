8f0a1f4012cd80401e909af9fcb396e22931e454
afaanashiq <afaan.ashiq@eigentech.com>
2022-05-06 21:06:36 +0100

    add type hints

--- middleware.py
+++ middleware.py
@@ -1,3 +1,5 @@
+from typing import Callable
+
 import webob
 
 
@@ -5,21 +7,23 @@ class Middleware:
     def __init__(self, app):
         self.app = app
 
-    def __call__(self, environ, start_response):
+    def __call__(self, environ, start_response) -> webob.Response:
         request = webob.Request(environ)
         response = self.app.handle_request(request)
         return response(environ, start_response)
 
-    def add(self, middleware_cls):
+    def add(self, middleware_cls: Callable) -> None:
         self.app = middleware_cls(self.app)
 
-    def process_request(self, req):
+    def process_request(self, request: webob.Request) -> webob.Request:
         ...
 
-    def process_response(self, req, resp):
+    def process_response(
+        self, request: webob.Request, response: webob.Response
+    ) -> webob.Response:
         ...
 
-    def handle_request(self, request):
+    def handle_request(self, request: webob.Request) -> webob.Response:
         self.process_request(request)
         response = self.app.handle_request(request)
         self.process_response(request, response)
