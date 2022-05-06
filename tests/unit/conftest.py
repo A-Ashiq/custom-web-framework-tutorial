6d764476fee2e3daab1ddf49490142e774ba4f6c
afaanashiq <afaan.ashiq@eigentech.com>
2022-05-06 21:35:15 +0100

    add tests for orm

---
+++ tests/unit/conftest.py
@@ -0,0 +1,44 @@
+# conftest.py
+
+import os
+import pytest
+
+from api import API
+from orm import Database, Table, Column, ForeignKey
+
+
+@pytest.fixture
+def api():
+    return API()
+
+
+@pytest.fixture
+def client(api):
+    return api.test_session()
+
+
+@pytest.fixture
+def db():
+    DB_PATH = "./test.db"
+    if os.path.exists(DB_PATH):
+        os.remove(DB_PATH)
+    return Database(DB_PATH)
+
+
+@pytest.fixture
+def Author():
+    class Author(Table):
+        name = Column(str)
+        age = Column(int)
+
+    return Author
+
+
+@pytest.fixture
+def Book(Author):
+    class Book(Table):
+        title = Column(str)
+        published = Column(bool)
+        author = ForeignKey(Author)
+
+    return Book
